293fd9469f80533bd22c6bc522719da61a9485f9
dokdo <jcs191072@gmail.com>
2020-11-25 21:26:14 +0900

    crowdFunding 테스트 코드 추가

--- crowdFunding/test/CrowdFunding.test.js
+++ crowdFunding/test/CrowdFunding.test.js
@@ -26,19 +26,20 @@ beforeEach(async () => {
     });
 
   await factory.methods
-    .createCrowdFunding('1')
-    .send({ from: accounts[0], gas: '1000000' });
+    .createCrowdFunding('10000') // wei로 전달
+    .send({
+      from: accounts[0],
+      gas: '1000000',
+    });
 
   [
     crowdFundingAddress,
   ] = await factory.methods.getDeployedCrowdFundings().call();
-  console.log('crowdFudingAddress', crowdFundingAddress);
 
   crowdFunding = await new web3.eth.Contract(
     JSON.parse(compiledCrowdFunding.interface),
     crowdFundingAddress
   );
-  console.log('crowdFunding.options.address', crowdFunding);
 });
 
 describe('CrowdFunding', () => {
@@ -47,5 +48,126 @@ describe('CrowdFunding', () => {
   });
   it('deploys a crowdFunding', () => {
     assert.ok(crowdFunding.options.address);
+  });
+
+  it('verifies whether the crowdFunding manager is a correct person', async () => {
+    const manager = await crowdFunding.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('allows people to contribue money and marks them as contributors', async () => {
+    await crowdFunding.methods.contribute().send({
+      value: '10001',
+      from: accounts[1],
+    });
+    const isContributor = await crowdFunding.methods
+      .contributors(accounts[1])
+      .call();
+
+    assert(isContributor);
+  });
+
+  it('requires a minimum contribution', async () => {
+    try {
+      await crowdFunding.methods.contribute().send({
+        value: '1000',
+        from: accounts[1],
+      });
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
+
+  it('allows a manager to make a paymenet request', async () => {
+    await crowdFunding.methods
+      .createRequest('Buy a car', '100000', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+    const request = await crowdFunding.methods.requests(0).call();
+    assert.equal('Buy a car', request.description);
+  });
+
+  it('processes requests successfully', async () => {
+    await crowdFunding.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether'),
+    });
+    await crowdFunding.methods
+      .createRequest('Buy a car', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+
+    await crowdFunding.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000',
+    });
+
+    await crowdFunding.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000',
+    });
+
+    let accountTwoBalance = await web3.eth.getBalance(accounts[1]);
+    accountTwoBalance = web3.utils.fromWei(accountTwoBalance, 'ether');
+    accountTwoBalance = parseFloat(accountTwoBalance);
+    assert(accountTwoBalance > 104);
+  });
+
+  it('finalizes requests without approving', async () => {
+    await crowdFunding.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether'),
+    });
+
+    await crowdFunding.methods
+      .createRequest('Buy a car', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+
+    try {
+      await crowdFunding.methods.finalizeRequest(0).send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
+  });
+
+  it('should fail when a non-manager tries to finalize requests', async () => {
+    await crowdFunding.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether'),
+    });
+    await crowdFunding.methods
+      .createRequest('Buy a car', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000',
+      });
+
+    await crowdFunding.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000',
+    });
+
+    try {
+      await crowdFunding.methods.finalizeRequest(0).send({
+        from: accounts[5],
+        gas: '1000000',
+      });
+      assert(false);
+    } catch (error) {
+      assert(error);
+    }
   });
 });
